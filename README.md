f58d70510e82b67fa1a84a870fc878c9cb3a981e
Christine Yoo <christinecho.s@gmail.com>
2021-01-28 17:00:09 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NASA API App
+
+Education | Astronomy | API Fun
+
+This app enables users to see the Astronomy Picture of the Day (APOD) and learn more about outer space by searching terms in the NASA image and video library.
---
+++ main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const apiKey = "IptJpzC6QdzZTggB5pgKm6BLb4B8b8mjoHktBJgf";
+const searchURL = "https://api.nasa.gov/planetary/apod";
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
+}
+
+function displayResults(responseJson) {
+  // if there are previous results, remove them
+  $("#results-list").empty();
+    for (let i = 0; i < responseJson.data.length; i++) {
+        $("#results-list").append(
+          `<li><h3>${responseJson.data[i].fullName}</h3>
+            <p>${responseJson.data[i].description}</p>
+            <p>Learn more: <a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a></p>
+            </li><hr>`
+        );
+      }
+  //display the results section
+  $("#results").removeClass("hidden");
+}
+
+function getParks(query, maxResults) {
+  const params = {
+    api_key: apiKey,
+    q: query,
+    limit: maxResults,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + "?" + queryString;
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayResults(responseJson))
+    .catch((err) => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    const searchTerm = $("#js-search-term").val();
+    const maxResults = $("#js-max-results").val();
+    getParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
